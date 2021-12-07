c5dbc25b244e03475807c909319f1272893e24d8
Lakshay <lakshayyadav2006@gmail.com>
2021-12-07 14:51:00 +0530

    Commented the file

--- main.py
+++ main.py
@@ -8,14 +8,19 @@ valid_inputs = encode_inputs + decode_inputs
 
 def caesar(cipher_direction, start_text, shift_amount):
     end_text = ""
+    # if the direction is valid, then make the shift amount negative
     if cipher_direction in decode_inputs:
         shift_amount *= -1
     for char in start_text:
         if char in alphabets:
+            # get the index
             index = alphabets.index(char)
+            # shift the index
             new_index = index + shift_amount
+            # add the alphabet at the given index to the end string
             end_text += alphabets[new_index]
         else:
+            # if the char is not an alphabet, then add it as it is without shifting
             end_text += char
     print(f"\nText after {cipher_direction}ing is: {end_text}\n")
 
@@ -29,8 +34,7 @@ while should_continue:
     shift = int(input("Enter the shift number: ")) % 26
     caesar(direction, text, shift)
 
-    result = input(
-        "Type 'yes' if you want to go again. Type 'no' if you don't: ")
-    if result == "no":
+    if input("Do you want to go again? (y/n): ").casefold() != "y":
         should_continue = False
         print("\nExiting.")
+    
